08c5f2b4213bf0877dbb85a87be1e532271f059e
jonathangfaggiani <jonathangfaggiani@2e462350-27fc-ce00-b00a-9bd1ef852287>
2012-04-13 00:24:20 +0000

    Vista registro pauta

--- trunk/Fuentes/Ventas/Ventas.DAT/ConexionUtil.cs
+++ trunk/Fuentes/Ventas/Ventas.DAT/ConexionUtil.cs
@@ -15,8 +15,8 @@ namespace Ventas.DAL
         {
             if (_CadenaConexion == null)
             {
-                _CadenaConexion = "Server=VIRTUALXP-50904\\SQL2008; User ID=sa; Initial Catalog=Ventas; password=royal2008;";
-                //_CadenaConexion = "Data Source = (local); Initial Catalog = Ventas; Integrated Security = SSPI;";
+                //_CadenaConexion = "Server=VIRTUALXP-50904\\SQL2008; User ID=sa; Initial Catalog=Ventas; password=royal2008;";
+                _CadenaConexion = "Data Source = (local); Initial Catalog = Ventas; Integrated Security = SSPI;";
             }
             return _CadenaConexion;
         }
